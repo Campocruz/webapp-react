e2a4aa49f55da666ff237845b7b533873276abc0
Campocruz <cosmacampo93@yahoo.it>
2026-07-07 20:55:53 +0200

    create a form and add function to store the new reviws

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Esercizio
+# Esercizio 1
 Ora è il momento di prepararci al frontend della nostra Web App!
 
 ## MILESTONE 0 
@@ -29,3 +29,21 @@ Ora è il momento di prepararci al frontend della nostra Web App!
 
 ## Bonus
 - Curare l’aspetto estetico della vostra applicazione
+
+# Esercizio 2
+- Miglioriamo l’esperienza dell’utente inserendo 
+
+## MILESTONE 1 (BACKEND)
+
+- Predisponiamo un’API per salvare nel database una nuova recensione legata ad un film
+- Testiamola su postman e verifichiamo che nel DB venga effettivamente inserita una nuova recensione
+
+## MILESTONE 2 (FRONTEND)
+
+- Creiamo un componente che contenga il form per le recensioni
+- Inseriamo questo componente nella pagina di dettaglio del film
+- All’invio del form, la nuova recensione viene salvata sul database e visualizzata nella pagina, in fondo alle altre
+
+## BONUS:
+
+- Inseriamo una validazione nel form di recensione
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -3,14 +3,35 @@ import { useParams } from "react-router"
 import ReviewCard from "../components/ReviewCard/ReviewCard";
 
 export default function MovieDetail() {
+
+  const initialObj = {
+    name: '',
+    vote: '',
+    text: ''
+  }
+
   const { id } = useParams();
   const url = `http://127.0.0.1:3000/api/v1/mobies/${id}`;
   const [movie, setMovie] = useState({});
+  const [inputForm, setInputForm] = useState(initialObj)
 
   function fetchData(url) {
     fetch(url)
       .then(response => response.json())
       .then(data => setMovie(data))
+      .catch(err => console.log(err))
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log(inputForm);
+    fetch(url + '/reviews', {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(inputForm)
+    })
+      .then(response => response.json())
+      .then(data => setInputForm(initialObj))
       .catch(err => console.log(err))
   }
 
@@ -35,6 +56,43 @@ export default function MovieDetail() {
                     <li>Genre: {movie.genre}</li>
                     <li>Year: {movie.release_year}</li>
                   </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div className="accordion" id="accordion">
+            <div className="accordion-item">
+              <h2 className="accordion-header">
+                <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  Add Your Comment
+                </button>
+              </h2>
+              <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordion">
+                <div className="accordion-body">
+                  <form className="row g-3" onSubmit={handleSubmit}>
+                    <div className="col-md-6">
+                      <label htmlFor="name" className="form-label">Username</label>
+                      <input type="text" className="form-control" id="name" name="name" value={inputForm.name} onChange={(e) => setInputForm({ ...inputForm, name: e.target.value })} />
+                    </div>
+                    <div className="col-md-6">
+                      <label htmlFor="inputState" className="form-label">Rating</label>
+                      <select id="inputState" defaultValue={null} className="form-select" onChange={(e) => setInputForm({ ...inputForm, vote: parseInt(e.target.value) })}>
+                        <option value={null}>...</option>
+                        <option value={1}>1</option>
+                        <option value={2}>2</option>
+                        <option value={3}>3</option>
+                        <option value={4}>4</option>
+                        <option value={5}>5</option>
+                      </select>
+                    </div>
+                    <textarea className="form-control" value={inputForm.text} onChange={(e) => setInputForm({ ...inputForm, text: e.target.value })}></textarea>
+                    <div className="col-12">
+                      <button type="submit" className="btn btn-primary">ADD</button>
+                    </div>
+                  </form>
                 </div>
               </div>
             </div>
